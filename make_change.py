9a2eed4b2704f881e35f2079cc4f9fa942d24a39
Steve Zelaznik <zelaznik@yahoo.com>
2015-08-26 21:12:59 -0700

    Removed extraneous is_sorted check.

--- make_change.py
+++ make_change.py
@@ -5,7 +5,6 @@ def is_sorted_desc(arr):
     return True
 
 def _make_change(amount, coins = ()):
-    assert is_sorted_desc(coins)
     if amount < 0:
         raise ValueError("Amount must be non-negative.")
     elif (amount > 0) and (not coins):
@@ -22,7 +21,7 @@ def _make_change(amount, coins = ()):
         len_with_first_coin = len(change_with_first_coin)
         success = True
     except ValueError:
-        len_with_first_coin = float('inf')    
+        len_with_first_coin = float('inf')
 
     try:
         change_drop_first_coin = _make_change(amount, coins[1:])
@@ -39,7 +38,7 @@ def _make_change(amount, coins = ()):
         return change_with_first_coin
     else:
         return change_drop_first_coin
-    
+
 def make_change(amount, coins=()):
     assert is_sorted_desc(coins), "Coins not sorted in desc order."
     return _make_change(amount, coins)

7197a9818a072d20058c9504511ff3a59af04c4b
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-14 22:57:58 -0700

    Fixed password validation (minimum was spelled wrong).  Updated routes.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   validates :username, :password_digest, :session_token, presence: true
   validates :username, :session_token, uniqueness: true
 
-  validates :password, length: {minimun: 6, allow_nil: true}
+  validates :password, length: {minimum: 6, allow_nil: true}
   after_initialize :ensure_session_token
 
   attr_reader :password

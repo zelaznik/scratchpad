e3adc6c20abb36c0e251c9570788c1bbdd68dc79
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-15 00:48:30 -0700

    Added name attributes to the login inputs.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
-    debugger
   end
 
   def create
-    debugger
     @user = User.new(user_params)
-    if @user.save && details.save
+    if @user.save
       sign_in @user
       redirect_to root_url
     else

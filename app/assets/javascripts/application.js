d2ae5d2e1e49543c1421d8958b9f01ccf14628c7
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-15 01:39:33 -0700

    Added composite view and get_or_fetch.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,13 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-
-
+//= require underscore
+//= require backbone
+//= require_tree ./utils
+//= require progress
+//= require_tree ../templates
+//= require_tree ./models
+//= require_tree ./collections
+//= require_tree ./views
+//= require_tree ./routers
 //= require_tree .
---
+++ app/assets/javascripts/utils/get_or_fetch.js
@@ -0,0 +1,15 @@
+Backbone.Collection.getOrFetch = function(id) {
+  var item = this.get(id);
+  if (!item) {
+    item = new this.model({id: id});
+    item.fetch({
+      success: function() {
+        this.add(item);
+      }
+    });
+  } else {
+    item.fetch();
+  }
+
+  return item;
+};

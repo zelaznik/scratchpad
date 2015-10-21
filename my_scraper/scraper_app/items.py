30aae01dfc380f8802e173c4b272288494899ac4
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-21 12:39:57 -0700

    Added livingsocial_spider file + .gitignore files.

--- my_scraper/scraper_app/items.py
+++ my_scraper/scraper_app/items.py
@@ -1,1 +1,10 @@
 from scrapy.item import Item, Field
+
+class LivingSocialDeal(Item):
+    ''' Living social container (dictionary-like object) for scraped data. '''
+    title = Field()
+    link = Field()
+    location = Field()
+    original_price = Field()
+    price = Field()
+    end_date = Field()

b62e5cc102880af5074458eb0b4b29e24e8250a5
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-21 13:31:32 -0700

    Copied the parse function

--- my_scraper/scraper_app/spiders/livingsocial_spider.py
+++ my_scraper/scraper_app/spiders/livingsocial_spider.py
@@ -21,3 +21,30 @@ class LivingSocialSpider(BaseSpider):
         'price': './/a/div[@class="deal-prices"]/div[@class="deal-price"]/text()',
         'end_date': './/span[@itemscope]/meta[@itemprop="availabilityEnds"]/@content'
     }
+
+    def parse(self, response):
+        '''
+            Default callback used by Scrapy to process downloaded responses
+
+            Testing contracts:
+            @url http://www.livingsocial.com/cities/15-san-francisco
+            @returns items 1
+            @scrapes title link
+        '''
+
+        selector = HtmlXPathSelector(response)
+
+        #iterate over deals
+        for deal in selector.select(self.deals_list_xpath):
+            loader = XPathItemLoader(LivingSocialDeal(), selector=deal)
+
+            # define processors
+            loader.default_input_processor = MapCompose(unicode.strip)
+            loader.default_output_processor = Join()
+
+            # iterate over fields and add xpaths to the loader
+            for field, xpath in self.item_fields.items():
+                loader.add_xpath(field, xpath)
+            yield loader.load_item()
+
+        

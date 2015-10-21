47117863563512ce8ae6919a69cf2d34eac89110
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-21 13:27:58 -0700

    Added extra scrapy import modules.

---
+++ my_scraper/scraper_app/spiders/livingsocial_spider.py
@@ -0,0 +1,23 @@
+from scrapy.spider import BaseSpider
+from scrapy.selector import HtmlXPathSelector
+from scrapy.contrib.loader import XPathItemLoader
+from scrapy.contrib.loader.processor import Join, MapCompose
+
+from scraper_app.items import LivingSocialDeal
+
+class LivingSocialSpider(BaseSpider):
+    ''' Spider for regularly updated livingsocial.com site, San Francisco Page. '''
+
+    name = 'livingsocial'
+    allowed_domains = ['livingsocial.com']
+    start_urls = ['http://www.livingsocial.com/cities/15-san-francisco']
+
+    deals_list_xpath = '//li[@dealid]'
+    item_fields = {
+        'title': './/span[@itemscope]/meta[@itemprop="name"]/@content',
+        'link': './/a/@href',
+        'location': './/a/div[@class="deal-details"]/p[@class="location"]/text()',
+        'original_price': './/a/div[@class="deal-prices"]/div[@class="deal-strikethrough-price"]/div[@class="strikethrough-wrapper"]/text()',
+        'price': './/a/div[@class="deal-prices"]/div[@class="deal-price"]/text()',
+        'end_date': './/span[@itemscope]/meta[@itemprop="availabilityEnds"]/@content'
+    }

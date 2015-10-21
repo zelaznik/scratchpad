6cc08bbac4e06e7351c9957661183666e5ca9d51
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-21 13:39:48 -0700

    Moved db password to private file which is mentioned in .gitignore.

---
+++ my_scraper/scraper_app/settings.py
@@ -0,0 +1,10 @@
+BOT_NAME = 'livingsocial'
+SPIDER_MODULES = ['scraper_app.spiders']
+
+DATABASE = {
+    'drivername': 'postgres',
+    'host': 'localhost',
+    'port': '5432',
+    'username': 'zMac',
+    'password': 'postgres'
+}

5896216077eb1e08e81f26665ef409ff0c311c1b
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-25 14:55:14 -0700

    Got rid of html files in the public folder.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root to: "static_pages#index"
-  get 'index', to: "static_pages#index"
-  get 'about', to: "static_pages#about"
-  get 'home', to: 'static_pages#home'
-  get 'contact', to: 'static_pages#contact'
+  root to: "static_pages#home"
+  get 'index.html', to: "static_pages#home"
+  get 'home.html', to: "static_pages#home"
+  get 'aboutus.html', to: "static_pages#about"
+  get 'contactus.html', to: 'static_pages#contact'
 
   get 'scratchpad', to: 'static_pages#scratchpad'
 end

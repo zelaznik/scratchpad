7bd4633e0ae7b4668343a2689a5daf759c32785c
Steve Zelaznik <zelaznik@yahoo.com>
2015-10-25 12:26:06 -0700

    Added scratchpad html file for playing around with stuff.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ Rails.application.routes.draw do
   get 'about', to: "static_pages#about"
   get 'home', to: 'static_pages#home'
   get 'contact', to: 'static_pages#contact'
+
+  get 'scratchpad', to: 'static_pages#scratchpad'
 end

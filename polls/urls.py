d72b2b7ffb1306eb30337f1569482f5bcd7cae4c
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-09 21:11:14 -0800

    Fixed routing error.  Onto template error.

--- polls/urls.py
+++ polls/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
+    # ex: /polls/5/detail/
+    url(r'^(?P<question_id>[0-9]+)/detail/$', views.detail, name='detail'),
+
 ]

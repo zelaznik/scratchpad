5061e1efef22b9e0693f2a6e32713da2e8fda9a4
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-09 20:25:10 -0800

    List admin display.

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question, Choice
 
-class ChoiceInline(admin.TubularInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -12,6 +12,8 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Misc',               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date']}),
     ]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)

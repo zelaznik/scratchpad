5717db450a90060e6f23928f66f770ff8893ea7e
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-09 20:16:42 -0800

    Changed inspect fcn from __unicode__ to __str__.

--- polls/models.py
+++ polls/models.py
@@ -6,7 +6,7 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
-    def __unicode__(self):
+    def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
@@ -18,5 +18,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __unicode__(self):
+    def __str__(self):
         return self.choice_text

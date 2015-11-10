b430dc1d2face6be17f8a8ca22eeb3d78bf2b712
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-09 20:56:11 -0800

    Trivial index polls view.

--- polls/views.py
+++ polls/views.py
@@ -3,9 +3,12 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, world.  You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = '<br>'.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s. "

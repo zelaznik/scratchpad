361cf4c9eae1e434266a25e767358f7479be25a7
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-09 21:21:45 -0800

    Added detail form.

--- polls/views.py
+++ polls/views.py
@@ -9,8 +9,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s. "
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)

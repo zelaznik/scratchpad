7cb6bbd79087b3438db0485b6924bc412d29f822
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-09 21:39:02 -0800

    Fixed routes, replacing <question_id> with <pk> for generic views.

--- polls/urls.py
+++ polls/urls.py
@@ -8,15 +8,15 @@ urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
 
     # ex: /polls/5/results/
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
 
     # ex: /polls/5/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 
     # ex: /polls/5/detail/
-    url(r'^(?P<question_id>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
     # added the word 'specifics'
-    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^specifics/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 
 ]

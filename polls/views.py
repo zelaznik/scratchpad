9bad2914257e4b1aea01464fdf629f9a67565489
Steve Zelaznik <zelaznik@yahoo.com>
2015-11-09 21:02:35 -0800

    Used render shortcut.

--- polls/views.py
+++ polls/views.py
@@ -7,11 +7,8 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s. "

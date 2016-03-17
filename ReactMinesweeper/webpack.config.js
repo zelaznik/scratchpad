626be2c3feb2a82d605998a4a4625ff6bf6cf874
Steve Zelaznik <zelaznik@yahoo.com>
2016-03-17 18:39:43 -0400

    Installed babel extensions.

--- ReactMinesweeper/webpack.config.js
+++ ReactMinesweeper/webpack.config.js
@@ -4,5 +4,19 @@ module.exports = {
   output: {
     path: "./",
     filename: "bundle.js"
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
   }
+
 };
